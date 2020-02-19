c6b0ab94b36a52ec4aa1c2b3b7da7c1edd13e263
Maksim Tarasyuk <mtarasyuk@agranovsky.org>
2020-02-19 19:16:28 +0300

    Layout update

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    const sendButton = document.querySelector('.send-button');
+
+    sendButton.addEventListener('click', onClick);
+
+    function onClick (e) {
+
+    }
+};
