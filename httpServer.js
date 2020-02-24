122f4e3a9564fd72c1e13a5b833e1943e8df9245
Makc <atlanticrev@gmail.com>
2020-02-24 13:16:11 +0300

    dev server added

--- httpServer.js
+++ httpServer.js
@@ -1,13 +1,31 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
-const websocketServer = http.createServer((request, response) => {
+const app = express();
+
+/* @todo как точно работает express.static? */
+app.use('/build', express.static('build'));
+
+app.get('/', (request, response) => {
     console.log(`request -> `, request.url);
-    response.writeHead(200, {'Content-type': 'text/html'});
-
-    const readStream = fs.createReadStream(`${__dirname}/index.html`, 'utf8');
-    readStream.pipe(response);
+    response.sendFile(`${__dirname}/index.html`);
 });
 
-websocketServer.listen(3000);
-console.log(`listen -> port: 3000`);
+//start our websocketServer
+app.listen(3000, () => {
+    console.log(`listen -> port: 3000`);
+});
+
+// Without Express
+// const http = require('http');
+// const fs = require('fs');
+//
+// const websocketServer = http.createServer((request, response) => {
+//     console.log(`request -> `, request.url);
+//     response.writeHead(200, {'Content-type': 'text/html'});
+//
+//     const readStream = fs.createReadStream(`${__dirname}/index.html`, 'utf8');
+//     readStream.pipe(response);
+// });
+//
+// websocketServer.listen(3000);
+// console.log(`listen -> port: 3000`);
